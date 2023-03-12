0e6237c3fbeed95763e2444070a224015711f101
stivenseraphic <stivenb@seraphicsecurity.com>
2023-03-12 02:50:17 +0100

    fixes

--- src/Layouts/Header.tsx
+++ src/Layouts/Header.tsx
@@ -1,9 +1,29 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import { Link } from "react-router-dom";
 
 import styles from "./header.module.scss";
 
 function Header() {
+  const navRef = useRef<HTMLDivElement>(null);
+
+  const handleUserScroll = () => {
+    const scrollTop = window.scrollY;
+
+    if (scrollTop > 80) {
+      navRef?.current?.classList.add("sticky");
+    } else {
+      navRef.current?.classList.remove("sticky");
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleUserScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleUserScroll);
+    };
+  }, []);
+
   const headerNavigation = [
     {
       redirect: "/",
@@ -18,7 +38,7 @@ function Header() {
   ];
 
   return (
-    <div className={styles.header}>
+    <div ref={navRef} className={styles.header}>
       <div className={styles.header__container}>
         {headerNavigation.map((link, linkIndex) => {
           return (
